a588ea2404aa515aa0544cb2b519b2b1822f5fac
ganesh-banda <gb.edu006@gmail.com>
2026-03-04 17:11:24 +0530

    Added corrected files

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import pandas as pd
+import requests
 
 from agent import reflex_agent
 
 app = Flask(__name__)
-CORS(app)   # Enables frontend to access API
+CORS(app)
 
-# Home route
 @app.route("/")
 def home():
     return "AQI Reflex Agent API Running"
 
-# AQI API route
 @app.route("/aqi")
 def get_aqi():
 
-    # Read sensor data
-    data = pd.read_csv("sensor_data.csv")
+    API_KEY = "a8bc7fcc0d461539d83668ce9d7389ed"
 
-    # Get latest reading
-    latest = data.iloc[-1]
+    lat = 17.3850
+    lon = 78.4867
 
-    pm25 = latest["PM25"]
+    url = f"https://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_KEY}"
 
-    # Run reflex agent
-    aqi, status = reflex_agent(pm25) 
+    response = requests.get(url)
+    data = response.json()
+
+    # Debug print to terminal
+    print(data)
+
+    if "list" not in data:
+        return jsonify({
+            "error": "API response invalid",
+            "response": data
+        })
+
+    pm25 = data["list"][0]["components"]["pm2_5"]
+
+    aqi, status = reflex_agent(pm25)
 
     return jsonify({
         "PM25": pm25,
         "AQI": aqi,
         "Status": status
     })
-
 
 if __name__ == "__main__":
     app.run(debug=True)
