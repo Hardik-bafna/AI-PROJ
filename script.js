e7b93bb03981e5f6ab937f0e46fe71324675c3e3
ganesh-banda <gb.edu006@gmail.com>
2026-03-04 17:36:08 +0530

    Added AQI

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-fetch("http://127.0.0.1:5000/aqi")
-.then(response => response.json())
-.then(data => {
+async function getAQI() {
 
-document.getElementById("aqi").innerText = data.AQI
-document.getElementById("status").innerText = data.Status
+    const city = document.getElementById("cityInput").value;
 
-})
+    if (!city) {
+        alert("Please enter a city");
+        return;
+    }
+
+    document.getElementById("aqi").innerText = "...";
+    document.getElementById("status").innerText = "Loading...";
+
+    const response = await fetch(`http://127.0.0.1:5000/aqi?city=${city}`);
+    const data = await response.json();
+
+    if (data.error) {
+        document.getElementById("aqi").innerText = "--";
+        document.getElementById("status").innerText = data.error;
+        return;
+    }
+
+    document.getElementById("aqi").innerText = data.AQI;
+    document.getElementById("status").innerText = data.Status;
+}
